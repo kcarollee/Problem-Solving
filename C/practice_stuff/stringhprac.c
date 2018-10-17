bcdd527e947763cf7964b44f9937131bc6017aa0
Dukwon Karl Lee <kcarollee31693@gmail.com>
2018-10-17 12:50:39 +0900

    practicing strings

--- C/practice_stuff/stringhprac.c
+++ C/practice_stuff/stringhprac.c
@@ -4,10 +4,12 @@
 
 int main()
 {
-    char string[len], string2[len], string3[len], *p1, *p2, *p3;
+    char string[len], string2[len], string3[len], string4[len], string5[len], *p1, *p2, *p3, *p4, *p5;
     p1 = string;
     p2 = string2;
     p3 = string3;
+    p4 = string4;
+    p5 = string5;
     gets(string);
     gets(string2);
     printf("%s\n", p1);
@@ -25,5 +27,12 @@ int main()
     printf("%s\n", p3);
     printf("%s\n", p2);
 
+    //comparing string4 and string5
+    printf("Enter string4: ");
+    gets(p4);
+    printf("Enter sting5: ");
+    gets(p5);
+    printf("%d\n", strcmp(p4, p5));
+    printf("%d\n", strcmp(p5, p4));
 
 }
