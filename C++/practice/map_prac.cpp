daf692d0cf9b181ca881d8cc85c9f4b14353d989
dukwon lee <kcarollee31693@gmail.com>
2019-02-06 15:35:01 +0900

    Adding

--- C++/practice/map_prac.cpp
+++ C++/practice/map_prac.cpp
@@ -1,4 +1,28 @@
 #include <iostream>
+#include <map>
 using namespace std;
 
+int main()
+{
+    map<string, int> player;
+    player.insert(make_pair("soccer", 11));
+    player.insert(make_pair("baseball", 9));
+    player.insert(make_pair("volleyball", 6));
+    player["football"] = 11;
+    player["basketball"] = 5;
+    player["handball"] = 7;
 
+    cout<<"before: "<<endl;
+    map<string, int>::iterator mt;
+    for (mt = player.begin(); mt != player.end(); mt++){
+        cout<<"Key: "<<mt -> first<<","<<"Value: "<<mt -> second<<endl;
+    }
+    cout<<endl;
+    player.erase("football");
+    mt = player.find("volleyball");
+    player.erase(mt);
+    cout<<"after: "<<endl;
+    for (auto it : player){
+        cout<<"key: "<<it.first<<","<<"value: "<<it.second<<endl;
+    }
+}
