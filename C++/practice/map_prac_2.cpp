b448f8adf301f075a503d3507986d606f9dd303d
dukwon lee <kcarollee31693@gmail.com>
2019-02-08 14:13:12 +0900

    Adding

--- C++/practice/map_prac_2.cpp
+++ C++/practice/map_prac_2.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+class Board
+{
+private:
+    int posX;
+    int posY;
+    vector<int>
+public:
+    Board(int x, int y) : posX(x), posY(y)
+    {
+
+    }
+
+};
--- Python/practice_stuff/intermediate_py/scratch.py
+++ Python/practice_stuff/intermediate_py/scratch.py
@@ -1,26 +1,8 @@
-class Calc:
-	def __init__(self, n):
-		self.until = n
-		self.sum1 = 0
+a = [["o","x","o"], ["x", "o", "x"]]
+print(len(set(a[0])))
 
-		self.Answer()
-		
-	def Answer(self):
 
-		self.sum1 = sum(range(1, self.until + 1))
-	
-	def Show(self):
-		print("%d %d %d" % (self.sum1, self.sum1 * 2 - self.until, self.sum1 * 2))
+a = 1 + 2 + 3 + \
+4 + 5 + 6
 
-def main():
-	test_case = int(input())
-	for i in range(test_case):
-		set_number, N = map(int, input().split(' '))
-		data = Calc(N)
-		print(set_number, end = ' ')
-		data.Show()
-
-if __name__ == "__main__":
-	main()
-		
-		
+print(a)
